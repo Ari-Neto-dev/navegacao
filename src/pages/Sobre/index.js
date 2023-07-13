68974c79f1792ffeb786067cfa7a631b40add2e0
Ari Azevedo <azevedo.consultor@hotmail.com>
2023-07-13 17:52:20 -0300

    Contato

--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,10 +1,13 @@
-
+import{Link}from 'react-router-dom';
 
 
 function Sobre() {
   return (
     <div>
-     <h1> Página sobre a empresa!</h1>
+     <h1> Página sobre a empresa!</h1><br/><br/>
+
+     <Link to="/">Página Home</Link><br/>
+     <Link to="/contato">Contato</Link>
     </div>
   );
 }
